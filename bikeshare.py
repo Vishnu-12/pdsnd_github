6c9f8a00f5dbeddaeaeaf41cbf01c2fe68f54a33
Vishnu Priya <priyakodad@gmail.com>
2019-05-19 08:16:51 +0530

    style: Formatting the bikeshare.py file, no code change
    the cities are changed from 'chicago','new york city', 'washington' to 'CHICAGO', 'NEW YORK CITY', 'WASHINGTON'

--- bikeshare.py
+++ bikeshare.py
@@ -2,26 +2,19 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+CITY_DATA = { 'CHICAGO': 'chicago.csv',
+              'NEW YORK CITY': 'new_york_city.csv',
+              'WASHINGTON': 'washington.csv' }
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
+    
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+   
     while True:
  
         city = input('Which city would you like to analyse the  data for?(chicago, new york city, washington): ')
-        city = city.lower()
-        if city in ('chicago', 'new york city', 'washington'):
+        city = city.upper()
+        if city in ('CHICAGO', 'NEW YORK CITY', 'WASHINGTON'):
             print('thank you')  
             break
         else:
